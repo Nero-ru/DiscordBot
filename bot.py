3b6af0c9304585da97d0593ae8141c3b046b79c2
Nero <zheka.tk97@gmail.com>
2020-11-24 11:15:41 +1000

    Initial

---
+++ bot.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import  commands
+
+client = commands.Bot(command_prefix = '!')
+
+@client.event
+
+async def on_ready():
+    print('BOT connected')
+
+@client.command( pass_context = True)
+
+async def hello(ctx):
+    await ctx.send('Привет')
+
+#Connect
+
+token = open ('token.txt', 'r').readline()
+
+client.run(token)
